b0235fe36f09b66f82fd2f662d4bffc5a7c6399e
Panny Nhia Yi Ly <pannynhiayily66@gmail.com>
2024-10-10 21:38:14 -0700

    user class

--- src/budgetapp.java
+++ src/budgetapp.java
@@ -1,5 +1,23 @@
-public class budgetapp {
-    public static void main(String[] args) {
-        System.out.println("This is a test.");
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//user class
+class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return username;
+    }
+    public String getPass() {
+        return password;
     }
 }
+
+//class to manage user registration and login
+class User
