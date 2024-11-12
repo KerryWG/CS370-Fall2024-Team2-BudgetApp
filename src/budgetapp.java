4105adc27e14797bccd20feaced2f2d2c78a540d
Panny NhiaYiLy <165870031+pnhia@users.noreply.github.com>
2024-11-12 14:13:34 -0800

    finished password. next step - spring boot

--- src/budgetapp.java
+++ src/budgetapp.java
@@ -15,8 +15,33 @@ class User {
     public String getUser() {
         return username;
     }
-    public String hasingPass(String password) {
 
+    /**
+     * Hashing algorithm using SHA-256.
+     * Plain text gets hashed and returns as a Base64 encoded
+     * of the hash.
+     *
+     * @param password
+     * @return hashed password as a Base64 encoded string
+     */
+
+    public String hashPassword(String password) {
+        try {
+            //start to initialize the md object to use SHA-256 algorithm
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            //hash bytes
+            byte[] hashedBytes = md.digest(password.getBytes());
+            //encode bytes to Base64 string for storage and return
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            //handle error or exceptions with error message
+            System.out.println("SHA-256 algorithm unavailable. \nERROR");
+            return null;
+        }
+    }
+
+    public boolean checkPassword(String password) {
+        return this.passwordHash.equals(hashPassword(password));
     }
 }
 
